8548f57da84d07e7f53862bbb6682ad027024ae7
Yinghao Zhu <yhzhu99@gmail.com>
2023-08-10 13:48:07 +0800

    chore(lightgbm): use sklearn style, pass params

--- models/lightgbm.py
+++ models/lightgbm.py
@@ -1,4 +1,5 @@
 import lightgbm as lgb
+from lightgbm import LGBMClassifier
 
 
 class LightGBM():
@@ -6,20 +7,35 @@ class LightGBM():
         """params is a dict
         seed: int, random seed
         n_estimators: int, number of trees
+        learning_rate: float, learning rate
         max_depth: int, depth of trees
         """
         task = params['task']
         self.task = task
         seed = params['seed']
-        param = {'num_leaves': 31, 'objective': 'binary'}
-        param['metric'] = 'auc'
-        self.param = param
-
-        self.model = None
+        n_estimators = params['n_estimators']
+        learning_rate = params['learning_rate']
+        max_depth = params['max_depth']
+        if task == "outcome":
+            self.model = LGBMClassifier(random_state=seed, n_estimators=n_estimators, learning_rate=learning_rate, max_depth=max_depth, verbose=0)
+        elif task == "los":
+            pass
+        else:
+            raise ValueError("Task must be either 'outcome' or 'los'.")
 
     def fit(self, x, y):
-        train_data = lgb.Dataset(x, label=y[:, 0])
-        num_round = 100
-        self.model = lgb.train(self.param, train_data, num_round)
+        if self.task == "outcome":
+            self.model.fit(x, y[:, 0])
+        elif self.task == "los":
+            # self.model.fit(x, y[:, 1])
+            pass
+        else:
+            raise ValueError("Task must be either 'outcome' or 'los'.")
     def predict(self, x):
-        return self.model.predict(x)
+        if self.task == "outcome":
+            return self.model.predict_proba(x)[:, 1]
+        elif self.task == "los":
+            # return self.model.predict(x)
+            pass
+        else:
+            raise ValueError("Task must be either 'outcome' or 'los'.")
